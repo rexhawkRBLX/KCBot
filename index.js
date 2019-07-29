51f6aabe106621c28ca6b73e262a5caf5979c3b5
rexhawk <sparx460old3@gmail.com>
2019-07-29 19:45:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const botconfig = require("./botconfig.json");
+const bot_config = require("./botconfig.json");
 const express = require("express");
 const Discord = require("discord.js");
-const bottoken = process.env.token;
+const bot_token = process.env.token;
 const PORT = process.env.PORT || 3000;
 const app = express();
 const bot = new Discord.Client({disableEveryone: true});
@@ -10,61 +10,60 @@ app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
 
-bot.on("ready", async ()=> {
+bot.on("ready", async() => {
     console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity("a movie with rexhawk", {type: "WATCHING"});
+    await bot.user.setActivity("a movie", {type: "WATCHING"});
 });
 
 bot.on("message", async message => {
    if (message.author.bot) return;
    if (message.channel.type === "dm") return;
 
-   let prefix = botconfig.prefix;
+   let prefix = bot_config.prefix;
    let messageArray = message.content.split(" ");
    let cmd = messageArray[0];
    let args = messageArray.slice(1);
 
    if (cmd === `${prefix}serverinfo`){
-
-       let sicon = message.guild.iconURL;
-       let serverembed = new Discord.RichEmbed()
+       let s_icon = message.guild.iconURL;
+       let server_embed = new Discord.RichEmbed()
            .setDescription("Server Information")
            .setColor("#f54242")
-           .setThumbnail(sicon)
+           .setThumbnail(s_icon)
            .addField("Server name", message.guild.name)
            .addField("Created on", message.guild.createdAt)
            .addField("You joined",message.member.joinedAt)
            .addField("Total members", message.guild.memberCount);
 
-       return message.channel.send(serverembed);
+       return message.channel.send(server_embed);
    }
 
     if (cmd === `${prefix}botinfo`){
-        let bicon = bot.user.displayAvatarURL;
-        let botembed = new Discord.RichEmbed()
+        let b_icon = bot.user.displayAvatarURL;
+        let bot_embed = new Discord.RichEmbed()
             .setDescription("Bot Information")
             .setColor("#f54242")
-            .setThumbnail(bicon)
+            .setThumbnail(b_icon)
             .addField("Bot Name", bot.user.username)
             .addField("Created on", bot.user.createdAt);
-        return message.channel.send(botembed);
+        return message.channel.send(bot_embed);
     }
 
     if (cmd === `${prefix}say`){
         let localArgs = messageArray.slice(2);
-            let botembed = new Discord.RichEmbed()
+            let bot_embed = new Discord.RichEmbed()
                 .setColor(args[0])
                 .setDescription(localArgs.join(' '));
-            return message.channel.send(botembed);
+            return message.channel.send(bot_embed);
     }
     if (cmd === `${prefix}ssay`){
         let localArgs = messageArray.slice(2);
-        let botembed = new Discord.RichEmbed()
+        let bot_embed = new Discord.RichEmbed()
             .setColor(args[0])
             .setThumbnail("https://cdn.discordapp.com/attachments/579769933219758148/605478475033346081/Icon.png")
             .setDescription(localArgs.join(' '));
-        return message.channel.send(botembed);
+        return message.channel.send(bot_embed);
     }
 });
 
-bot.login(bottoken).catch(err => console.log(err));
+bot.login(bot_token).catch(err => console.log(err));
