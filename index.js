feb23846ede247867b4b06bd9cf2bde8db9f9a7d
rexhawkRBLX <sparx460old3@gmail.com>
2019-10-10 11:14:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -316,9 +316,9 @@ bot.on("message", async message => {
            .addField("Server Owner",message.guild.ownerID,true)
            .addField("Humans",message.guild.members.filter(member => !member.user.bot).size,true)
            .addField("Bots",message.guild.members.filter(member => member.user.bot).size,true)
+           .addField("Total members",message.guild.memberCount,true)
            .addField("Text Channels",message.guild.channels.size,true)
-           .addField("Text Channels",message.guild.roles.size,true)
-           .addField("Amount of members",message.guild.memberCount,true)
+           .addField("Roles",message.guild.roles.size,true)
            .setFooter(`${message.guild.id} • Created on ${dateformat}`)
            .setColor("#689FF5")
            .setAuthor(message.guild.name,bot.user.avatarURL);
