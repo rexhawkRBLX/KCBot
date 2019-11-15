2f1bba570675a0ec0d40e785f6413cfc4f4291d0
rexhawkRBLX <sparx460old3@gmail.com>
2019-11-15 14:25:35 -0500

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,12 +1,6 @@
 const Discord = require("discord.js");
-const fs = require("fs");
 
 module.exports.run = async (bot, message, args) => {
-    let totalFiles = 0;
-    fs.readdir("./", (error, files) => {
-        totalFiles = files.length;
-    });
-
     let dateformat = [bot.user.createdAt.getMonth()+1,
         bot.user.createdAt.getDate(),
         bot.user.createdAt.getFullYear()].join('/');
@@ -14,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
         .setAuthor(bot.user.tag,bot.user.avatarURL)
         .addField("Bot Name", bot.user.username,true)
         .addField("Created on", `${dateformat}`,true)
-        .addField("Number of commands",String(totalFiles),true)
+        .addField("Number of commands","15",true)
         .setFooter(`${bot.user.id} • by rexhawk#0132`)
         .setColor("#689FF5");
     await message.delete();
