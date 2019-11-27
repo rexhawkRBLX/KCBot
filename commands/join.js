04c0df13afd040bbd4d35c00c4d1ac160b7ecba3
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-11-27 12:33:36 -0500

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,19 +1,50 @@
 const Discord = require("discord.js");
 
-
-
 module.exports.run = async (bot, message, args) => {
-    let allowedRole = message.guild.roles.find("name", "SSU");
-    if (String(message.guild.id) === "621475864546115611") {
-        if (message.member.roles.find(r => r.name === "SSU")) {
-            return message.reply(`**ERROR**: You already have the ssu role. If you wish to remove it, say \`>leave ssu\``);
+
+    if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}join`) {
+        await message.delete();
+        let richEmbed = new Discord.RichEmbed()
+            .setColor("#333333")
+            .setTitle("**Command:** >join")
+            .setDescription("**Description: **Join an optional role\n**Usage: **>join [role]\n**Example: **>join ssu");
+        return message.channel.send(richEmbed);
+    }
+    if (String(args[0]).toLowerCase() === "ssu" || String(args[0]).toLowerCase() === "qotd") {
+        let allowedRole;
+        if (String(args[0]).toLowerCase() === "ssu") {
+            allowedRole = message.guild.roles.find("name", "SSU");
         } else {
-            const guildMember = message.member;
-            guildMember.addRole(allowedRole);
-            return message.reply(`You have successfully joined \`SSU\`.`);
+            allowedRole = message.guild.roles.find("name", "QOTD");
+        }
+        if (String(message.guild.id) === "621475864546115611") {
+            if (message.member.roles.find(r => r.name === "SSU")) {
+                if (String(args[0]).toLowerCase() === "ssu") {
+                    return message.reply(`**ERROR**: You already have the ssu role. If you wish to remove it, say \`>leave ssu\``);
+                }
+            } else {
+                if (String(args[0]).toLowerCase() === "ssu") {
+                    const guildMember = message.member;
+                    guildMember.addRole(allowedRole);
+                    return message.reply(`You have successfully joined \`SSU\`.`);
+                }
+            }
+            if (message.member.roles.find(r => r.name === "QOTD")) {
+                if (String(args[0]).toLowerCase() === "qotd") {
+                    return message.reply(`**ERROR**: You already have the qtod role. If you wish to remove it, say \`>leave qotd\``);
+                }
+            } else {
+                if (String(args[0]).toLowerCase() === "qotd") {
+                    const guildMember = message.member;
+                    guildMember.addRole(allowedRole);
+                    return message.reply(`You have successfully joined \`QOTD\`.`);
+                }
+            }
+        } else {
+            return message.reply(`**ERROR**: \`Incorrect Server\``);
         }
     } else {
-        return message.reply(`**ERROR**: \`Incorrect Server\``);
+        return message.reply(`**ERROR**: \`Role not found or is not joinable.\``);
     }
 };
 
