0234a0fb92b3371762178386a7ec54ed856293fc
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-11-27 12:50:42 -0500

    added leave

--- commands/leave.js
+++ commands/leave.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}join`) {
+    if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}leave`) {
         await message.delete();
         let richEmbed = new Discord.RichEmbed()
             .setColor("#333333")
-            .setTitle("**Command:** >join")
-            .setDescription("**Description: **Join an optional role\n**Usage: **>join [role]\n**Example: **>join ssu");
+            .setTitle("**Command:** >leave")
+            .setDescription("**Description: **Leave an optional role\n**Usage: **>leave [role]\n**Example: **>leave ssu");
         return message.channel.send(richEmbed);
     }
     if (String(args[0]).toLowerCase() === "ssu" || String(args[0]).toLowerCase() === "qotd") {
@@ -20,32 +20,32 @@ module.exports.run = async (bot, message, args) => {
         if (String(message.guild.id) === "621475864546115611") {
             if (message.member.roles.find(r => r.name === "SSU")) {
                 if (String(args[0]).toLowerCase() === "ssu") {
-                    return message.reply(`**ERROR**: You already have the ssu role. If you wish to remove it, say \`>leave ssu\``);
-                }
-            } else {
-                if (String(args[0]).toLowerCase() === "ssu") {
-                    const guildMember = message.member;
                     try {
-                        guildMember.addRole(allowedRole);
-                        return message.reply(`You have successfully joined \`SSU\`.`);
+                        const guildMember = message.member;
+                        guildMember.removeRole(allowedRole);
+                        return message.reply(`You have successfully left \`SSU\`.`);
                     } catch (err) {
                         return message.reply(`**ERROR: ** ${err}`)
                     }
+                }
+            } else {
+                if (String(args[0]).toLowerCase() === "ssu") {
+                    return message.reply(`**ERROR**: You are not in the ssu role. If you wish to join it, say \`>join ssu\``);
                 }
             }
             if (message.member.roles.find(r => r.name === "QOTD")) {
                 if (String(args[0]).toLowerCase() === "qotd") {
-                    return message.reply(`**ERROR**: You already have the qtod role. If you wish to remove it, say \`>leave qotd\``);
-                }
-            } else {
-                if (String(args[0]).toLowerCase() === "qotd") {
-                    const guildMember = message.member;
                     try {
-                        guildMember.addRole(allowedRole);
-                        return message.reply(`You have successfully joined \`QOTD\`.`);
+                        const guildMember = message.member;
+                        guildMember.removeRole(allowedRole);
+                        return message.reply(`You have successfully left \`QOTD\`.`);
                     } catch (err) {
                         return message.reply(`**ERROR: ** ${err}`)
                     }
+                }
+            } else {
+                if (String(args[0]).toLowerCase() === "qotd") {
+                    return message.reply(`**ERROR**: You are not in the qotd role. If you wish to join it, say \`>join qotd\``);
                 }
             }
         } else {
@@ -57,5 +57,5 @@ module.exports.run = async (bot, message, args) => {
 };
 
 module.exports.help = {
-    name: "join"
+    name: "leave"
 };
