b9f162a29185c17328b533aaf9ba2cc831994f11
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 15:31:22 -0500

    emoji

---
+++ commands/emoji.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+function createEmoji(bot){
+    bot.guild.createEmoji('https://i.imgur.com/w3duR07.png', 'kcSuccess')
+        .then(useEmoji(bot))
+        .catch(console.error);
+}
+
+function useEmoji(bot) {
+    if (bot.emojis.find(emoji => emoji.name === "kcSuccess")){
+        let emoji = bot.emojis.find(emoji => emoji.name === "kcSuccess");
+        return message.channel.send(`${emoji}`);
+    } else {
+        createEmoji(bot)
+    }
+}
+
+module.exports.run = async (bot, message, args) => {
+    useEmoji(bot)
+};
+
+module.exports.help = {
+    name: "emoji"
+};
