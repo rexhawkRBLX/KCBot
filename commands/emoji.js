2663bc61ad46e8b517e944c4a2cceee07cc89239
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 18:50:55 -0500

    Update emoji.js

--- commands/emoji.js
+++ commands/emoji.js
@@ -1,11 +1,9 @@
 const Discord = require("discord.js");
 
 function createEmoji(bot,message){
-    console.log("Three");
-    console.log(message.guild.id);
     let role = message.guild.roles.find(role => role.name === "King City");
     if (role) {
-        message.guild.createEmoji('https://i.vgy.me/FsRyFm.png', 'kcSuccess', [role])
+        message.guild.createEmoji(require("./../retrieveInfo").kcEmoji.success, 'kcSuccess', [role])
             .then(function () {
                 useEmoji(bot, message);
             })
@@ -14,19 +12,15 @@ function createEmoji(bot,message){
 }
 
 function useEmoji(bot,message) {
-    if (message.guild.emojis.find(emoji => emoji.name === "kcSuccess")){
-        console.log("One");
-        message.channel.send(`Found`);
-        let emoji = bot.emojis.find(emoji => emoji.name === "kcSuccess");
+    let emoji = bot.emojis.find(emoji => emoji.name === "kcSuccess");
+    if (emoji){
         return message.channel.send(`${emoji}`);
     } else {
-        console.log("Two");
         createEmoji(bot,message)
     }
 }
 
 module.exports.run = async (bot, message, args) => {
-    console.log("Five");
     useEmoji(bot,message)
 };
 
