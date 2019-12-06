4ec8689c66633733ae11014e1dad45893f7174fd
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-06 12:57:50 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const emojiFile = require("../emoji.js");
 
 module.exports.run = async (bot, message, args) => {
     message.channel.send(`Now returning KCGuard's emojis.`);
-    
+
     let emojiCheer = await emojiFile.useEmoji(bot,message,"cheer");
     let emojiDirection = await emojiFile.useEmoji(bot,message,"direction");
     let emojiNeutral = await emojiFile.useEmoji(bot,message,"neutral");
@@ -11,12 +11,13 @@ module.exports.run = async (bot, message, args) => {
     let emojiSuccess = await emojiFile.useEmoji(bot,message,"success");
     let emojiThinking = await emojiFile.useEmoji(bot,message,"thinking");
 
-    message.channel.send(`Cheer: ${await emojiCheer}`);
-    message.channel.send(`Direction: ${await emojiDirection}`);
-    message.channel.send(`Neutral: ${await emojiNeutral}`);
-    message.channel.send(`Ponder: ${await emojiPonder}`);
-    message.channel.send(`Success: ${await emojiSuccess}`);
-    message.channel.send(`Thinking: ${await emojiThinking}`);
+    let returnedMessage = await message.channel.send(`Cheer: ${await emojiCheer}`);
+    await returnedMessage.edit(`${returnedMessage.content}\nDirection: ${await emojiDirection}`);
+    await returnedMessage.edit(`${returnedMessage.content}\nNeutral: ${await emojiNeutral}`);
+    await returnedMessage.edit(`${returnedMessage.content}\nPonder: ${await emojiPonder}`);
+    await returnedMessage.edit(`${returnedMessage.content}\nSuccess: ${await emojiSuccess}`);
+    await returnedMessage.edit(`${returnedMessage.content}\nThinking: ${await emojiThinking}`);
+    
     return message.channel.send(`Finished.`);
 };
 
