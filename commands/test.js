8b92df54542224f6284aa31568708b62220b2c91
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-06 10:49:19 -0500

    u

--- commands/test.js
+++ commands/test.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const emojiFile = require("../emoji.js");
 
 module.exports.run = async (bot, message, args) => {
-    let emoji = await emojiFile.useEmoji(bot,message,args[0]);
-    return message.channel.send(`${emoji}`);
+    await emojiFile.useEmoji(bot,message,args[0]);
+    return message.channel.send(`Finished.`);
 };
 
 module.exports.help = {
--- emoji.js
+++ emoji.js
@@ -1,38 +1,31 @@
 const emojiNames = {["cheer"]: "kcCheer",["direction"]: "kcDirection",["neutral"]: "kcNeutral",["ponder"]: "kcPonder",["success"]: "kcSuccess",["thinking"]: "kcThinking"};
 
 module.exports.useEmoji = async (bot,message,emoji) => {
-    let finishedEmoji;
-    message.channel.send("Checking to see if emoji exists already...");
-    let emojiValue = bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji]);
-    message.channel.send("Completed checking if emoji exists already.");
-    message.channel.send("Checking if emoji was found...");
-    if (emojiValue){
-        message.channel.send("Emoji exists");
-        finishedEmoji = emojiValue;
-    } else {
-        message.channel.send("Emoji was not found.");
-        message.channel.send("Checking to see if KCGuards role exists");
-        let role = message.guild.roles.find(role => role.name === "King City");
-        message.channel.send("Done checking to see if role exists.");
-        if (role) {
-            message.channel.send("Role exists");
-            message.channel.send("Creating emoji");
-            message.guild.createEmoji(require("./retrieveInfo").kcEmoji[emoji], emojiNames[emoji], [role])
-                .then(emoji => {
-                    message.channel.send("Emoji was created.");
-                    finishedEmoji = emoji;
-                })
-                .catch(error => {
-                    message.channel.send(`**Error: **${error}`);
-                });
-            message.channel.send("Test check");
-            await setTimeout(function () {
-                message.channel.send(`Finished timeout, returning emoji`);
-                return finishedEmoji;
-            }, 5000)
-        } else {
-            message.channel.send("Role does not exist.");
-        }
+    let role = message.guild.roles.find(role => role.name === "King City");
+    if (role) {
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["cheer"], emojiNames["cheer"], [role])
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`);
+            });
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["direction"], emojiNames["direction"], [role])
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`);
+            });
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["neutral"], emojiNames["neutral"], [role])
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`);
+            });
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["ponder"], emojiNames["ponder"], [role])
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`);
+            });
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["success"], emojiNames["success"], [role])
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`);
+            });
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["thinking"], emojiNames["thinking"], [role])
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`);
+            });
     }
-
 };
