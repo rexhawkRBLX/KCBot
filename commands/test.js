40c30b0115073a6454d78239f082abc8d41bac4c
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-06 12:51:38 -0500

    .

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,21 @@ const Discord = require("discord.js");
 const emojiFile = require("../emoji.js");
 
 module.exports.run = async (bot, message, args) => {
-    await emojiFile.useEmoji(bot,message,args[0]);
+    message.channel.send(`Now returning KCGuard's emojis.`);
+    
+    let emojiCheer = await emojiFile.useEmoji(bot,message,"cheer");
+    let emojiDirection = await emojiFile.useEmoji(bot,message,"direction");
+    let emojiNeutral = await emojiFile.useEmoji(bot,message,"neutral");
+    let emojiPonder = await emojiFile.useEmoji(bot,message,"ponder");
+    let emojiSuccess = await emojiFile.useEmoji(bot,message,"success");
+    let emojiThinking = await emojiFile.useEmoji(bot,message,"thinking");
+
+    message.channel.send(`Cheer: ${await emojiCheer}`);
+    message.channel.send(`Direction: ${await emojiDirection}`);
+    message.channel.send(`Neutral: ${await emojiNeutral}`);
+    message.channel.send(`Ponder: ${await emojiPonder}`);
+    message.channel.send(`Success: ${await emojiSuccess}`);
+    message.channel.send(`Thinking: ${await emojiThinking}`);
     return message.channel.send(`Finished.`);
 };
 
--- emoji.js
+++ emoji.js
@@ -1,31 +1,8 @@
 const emojiNames = {["cheer"]: "kcCheer",["direction"]: "kcDirection",["neutral"]: "kcNeutral",["ponder"]: "kcPonder",["success"]: "kcSuccess",["thinking"]: "kcThinking"};
 
 module.exports.useEmoji = async (bot,message,emoji) => {
-    let role = message.guild.roles.find(role => role.name === "King City");
-    if (role) {
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["cheer"], emojiNames["cheer"], [role])
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`);
-            });
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["direction"], emojiNames["direction"], [role])
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`);
-            });
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["neutral"], emojiNames["neutral"], [role])
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`);
-            });
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["ponder"], emojiNames["ponder"], [role])
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`);
-            });
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["success"], emojiNames["success"], [role])
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`);
-            });
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["thinking"], emojiNames["thinking"], [role])
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`);
-            });
+    let emojiValue = bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji]);
+    if (emojiValue){
+        return await emojiValue;
     }
 };
