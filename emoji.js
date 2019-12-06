4bf460cbadd0e544a0bdbcdc129ad69f0e623949
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 23:08:30 -0500

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,13 +1,15 @@
 const emojiNames = {["cheer"]: "kcCheer",["direction"]: "kcDirection",["neutral"]: "kcNeutral",["ponder"]: "kcPonder",["success"]: "kcSuccess",["thinking"]: "kcThinking"};
 
-function createEmoji(bot,message,emoji,_callback){
+function createEmoji(bot,message,emoji){
     let role = message.guild.roles.find(role => role.name === "King City");
     if (role) {
         message.guild.createEmoji(require("./retrieveInfo").kcEmoji[emoji], emojiNames[emoji], [role])
+            .then(function () {
+                useEmojiLocal(bot,message,emoji)
+            })
             .catch(error => {
                 message.channel.send(`**Error: **${error}`)
             });
-        _callback();
     }
 }
 
@@ -16,16 +18,11 @@ function useEmojiLocal(bot,message,emoji){
     if (emojiValue){
         return emojiValue;
     } else {
-        createEmoji(bot,message,emoji, function(){
-            return entryPoint(bot,message,emoji);
-        });
+        createEmoji(bot,message,emoji);
     }
 }
 
-function entryPoint(bot,message,emoji){
-    useEmojiLocal(bot,message,emoji)
-}
-
 module.exports.useEmoji = async (bot,message,emoji) => {
-    entryPoint(bot,message,emoji)
+    let Emoji = await useEmojiLocal(bot,message,emoji)
+    return Emoji
 };
