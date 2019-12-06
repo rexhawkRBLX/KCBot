df8034dc713ea10f75a9fcf58d2b5f7d54691e39
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 20:29:48 -0500

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -6,20 +6,20 @@ function createEmoji(bot,message,emoji){
     console.log("Six");
     if (role) {
         console.log("Seven");
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji["success"], emojiNames[emoji.lower], [role])
+        message.guild.createEmoji(require("./retrieveInfo").kcEmoji[emoji], emojiNames[emoji], [role])
             .catch(console.error);
     }
 }
 
 module.exports.useEmoji = async (bot,message,emoji) => {
     console.log("One");
-    let emojiValue = bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji.lower]);
+    let emojiValue = bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji]);
     console.log("Two");
     if (emojiValue){
         console.log("Three");
         return emojiValue;
     } else {
         await createEmoji(bot,message,emoji);
-        return bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji.lower]);
+        return bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji]);
     }
 };
