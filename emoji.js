4645f8941acbeb88a58240132c6fec2d5c610aeb
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 23:17:11 -0500

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,27 +1,19 @@
 const emojiNames = {["cheer"]: "kcCheer",["direction"]: "kcDirection",["neutral"]: "kcNeutral",["ponder"]: "kcPonder",["success"]: "kcSuccess",["thinking"]: "kcThinking"};
 
-function createEmoji(bot,message,emoji){
-    let role = message.guild.roles.find(role => role.name === "King City");
-    if (role) {
-        message.guild.createEmoji(require("./retrieveInfo").kcEmoji[emoji], emojiNames[emoji], [role])
-            .then(emoji => {
-                return emoji
-            })
-            .catch(error => {
-                message.channel.send(`**Error: **${error}`)
-            });
-    }
-}
-
-function useEmojiLocal(bot,message,emoji){
+module.exports.useEmoji = async (bot,message,emoji) => {
     let emojiValue = bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji]);
     if (emojiValue){
         return emojiValue;
     } else {
-        return createEmoji(bot,message,emoji);
+        let role = message.guild.roles.find(role => role.name === "King City");
+        if (role) {
+            message.guild.createEmoji(require("./retrieveInfo").kcEmoji[emoji], emojiNames[emoji], [role])
+                .then(emoji => {
+                    return emoji
+                })
+                .catch(error => {
+                    message.channel.send(`**Error: **${error}`)
+                });
+        }
     }
-}
-
-module.exports.useEmoji = async (bot,message,emoji) => {
-    return await useEmojiLocal(bot,message,emoji);
 };
