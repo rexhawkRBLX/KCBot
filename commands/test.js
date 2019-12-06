7ae2a31f271b7bb69afebb13e1df7050afc25a4b
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 20:05:18 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 const emoji = require("../emoji.js");
 
 module.exports.run = async (bot, message, args) => {
-   return useEmoji(bot,message,args[0])
+    let emoji = useEmoji(bot,message,args[0]);
+    if (emoji) {
+        return message.channel.send(`${emoji}`);
+    } else {
+        return message.channel.send("Error");
+    }
 };
 
 module.exports.help = {
