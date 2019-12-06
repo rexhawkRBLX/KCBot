882d6ff7eb1565b74d6c5fd6edbc85844d658519
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-05 22:33:25 -0500

    Update emoji.js

--- emoji.js
+++ emoji.js
@@ -1,28 +1,21 @@
 const emojiNames = {["cheer"]: "kcCheer",["direction"]: "kcDirection",["neutral"]: "kcNeutral",["ponder"]: "kcPonder",["success"]: "kcSuccess",["thinking"]: "kcThinking"};
 
 function createEmoji(bot,message,emoji){
-    console.log("Five");
     let role = message.guild.roles.find(role => role.name === "King City");
-    console.log("Six");
     if (role) {
-        console.log("Seven");
         message.guild.createEmoji(require("./retrieveInfo").kcEmoji[emoji], emojiNames[emoji], [role])
-            .then(emoji => {
-                return emoji
-            })
-            .catch(console.error);
+            .catch(error => {
+                message.channel.send(`**Error: **${error}`)
+            });
     }
 }
 
 module.exports.useEmoji = async (bot,message,emoji) => {
-    console.log("One");
     let emojiValue = bot.emojis.find(emojiValue => emojiValue.name === emojiNames[emoji]);
-    console.log("Two");
     if (emojiValue){
-        console.log("Three");
         return emojiValue;
     } else {
-        let newEmoji = await createEmoji(bot,message,emoji);
-        return newEmoji;
+        await createEmoji(bot,message,emoji);
+        return await module.useEmoji(bot,message,emoji);
     }
 };
