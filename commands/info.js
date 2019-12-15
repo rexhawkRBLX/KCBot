57971458fb61b0ec4bd7f8aa780faedf45715d94
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2019-12-15 13:09:53 -0500

    updated game link

--- commands/info.js
+++ commands/info.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("King City Groups", `[King City, California](https://www.roblox.com/groups/5210085/King-City-California)\n[California Highway Patrol](https://www.roblox.com/groups/4876110/California-Highway-Patrol-Coastal-Division)\n[King City Police Department](https://www.roblox.com/groups/4897046/King-City-Police-Department)\n[King City Fire Department](https://www.roblox.com/groups/4898855/King-City-Fire-Department)\n[King City DoPW](https://www.roblox.com/groups/5232362/King-City-Department-of-Public-Works)`,true)
         .addField("Discord Servers",`[King City, California](${require("./../retrieveInfo").getServerInvite["621475864546115611"]})\n[California Highway Patrol](${require("./../retrieveInfo").getServerInvite["569636982528016425"]})\n[King City Police Department](${require("./../retrieveInfo").getServerInvite["571777729448509440"]})\n[King City Fire Department](${require("./../retrieveInfo").getServerInvite["573681387094147085"]})\n[King City DoPW](${require("./../retrieveInfo").getServerInvite["632986602993614869"]})\n[King City CoC](${require("./../retrieveInfo").getServerInvite["544031918912569356"]})\n[District Attorney's Office](${require("./../retrieveInfo").getServerInvite["648336531911606272"]})`, true)
         .addBlankField()
-        .addField("King City Game",`[King City, California](https://www.roblox.com/games/3571375889/King-City-California)`);
+        .addField("King City Game",`[King City, California](https://www.roblox.com/games/4240465985/King-City-California)`);
     return message.reply(rich_Embed);
 };
 
