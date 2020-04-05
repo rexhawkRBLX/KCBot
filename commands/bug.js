69ba2592699b8c4963325706d115f418f8577f31
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-04-05 13:52:35 -0400

    bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -19,27 +19,27 @@ function calcTime() {
 
 module.exports.run = async (bot, message, args) => {
 
-  if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}suggest`) {
+  if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}bug`) {
       await message.delete();
       let richEmbed = new Discord.RichEmbed()
           .setColor("#333333")
-          .setTitle("**Command:** >suggest")
-          .setDescription("**Description: **Create a new suggestion card in the [suggestions trello](https://trello.com/b/2Aio6E06)\n**Usage: **>suggest [content]\n**Example: **>suggest a statue of rexhawk");
+          .setTitle("**Command:** >bug")
+          .setDescription("**Description: **Create a new bug report in the [bug report trello](https://trello.com/b/2Aio6E06)\n**Usage: **>bug [content]\n**Example: **>bug there is no rexhawk statue!");
       return await message.channel.send(richEmbed);
   } else {
     let localArgs = message.content.split(" ").slice(1);
 
     let data = {
         name: localArgs.join(' '),
-        desc: `Contributor: ${message.member.user.tag}\nDate / Time (EST): ${calcTime()}`,
-        idList: '5e1262520612231fb472979f', //REQUIRED
-        idLabels: ['5e8a029d7bfaf66944b95d10','5e1262fb07d03a18361ac073'],
+        desc: `Reporter: ${message.member.user.tag}\nDate / Time (EST): ${calcTime()}`,
+        idList: '5e1a3683e3009c458f017c7f', //REQUIRED
+        idLabels: ['5e1a3683e3009c458f017c7f'],
     };
 
     Trello.card.create(data).then(function (response) {
       let richEmbed = new Discord.RichEmbed()
           .setColor("#008B8B")
-          .setDescription(`Complete! You can find your new suggestion [here](${response.shortUrl}).`);
+          .setDescription(`Complete! You can find your bug report [here](${response.shortUrl}).`);
       return message.channel.send(richEmbed);
       // console.log('response ', response);
     }).catch(function (error) {
