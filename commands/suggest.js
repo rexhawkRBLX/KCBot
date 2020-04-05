da06f9f53fa8d670ecc24c7546c59edd1ed3a529
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-04-05 10:09:05 -0400

    suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if (message.member.roles.has(637802573914570752)){
+  if (message.member.roles.has("637802573914570752")){
     return await message.reply(`found role`);
   } else {
     return await message.reply(`role was not found`);
