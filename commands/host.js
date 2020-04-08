6f38cb3291edf20ba6039fc83fd0dcf3a6249d54
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-04-07 20:37:38 -0400

    host.js

--- commands/host.js
+++ commands/host.js
@@ -1,14 +1,26 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  let localArgs =  message.content.split(" ").slice(1).join(' ');
 
-  let testMsg = localArgs.search("test");
-  if (testMsg === "test") {
-    return await message.reply("Found string")
+
+
+  //let localArgs =  message.content.split(" ").slice(1).join(' ');
+  if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}host`) {
+    await message.delete();
+    let richEmbed = new Discord.RichEmbed()
+        .setColor("#333333")
+        .setTitle("**Command:** >host")
+        .setDescription("**Description: **Host an event\n**Usage: **>host [event type] {[arguments]}\n**Example: **>host ssu {host:@Rexhawk} {msg:lets get active!}");
+    return message.channel.send(richEmbed);
   } else {
-    return await message.reply("String not found " + testMsg)
+    let testMsg = localArgs.search("test");
+    if (testMsg === 0) {
+      return await message.reply("Found string")
+    } else {
+      return await message.reply("String not found " + testMsg)
+    }
   }
+
   /*
     if(message.member.roles.find(r => r.name === "Administrative Services") || message.member.roles.find(r => r.name === "Cabinet") || message.member.roles.find(r => r.name === "Department Command") ||message.member.roles.find(r => r.name === "Mayor") || message.member.roles.find(r => r.name === "Assistant Mayor")){
         if (String(message.guild.id) === "621475864546115611") {
