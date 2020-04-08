d11e4edc5ec553b706ee3936cc0fd7edc4e178f6
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-04-07 20:42:58 -0400

    host.js

--- commands/host.js
+++ commands/host.js
@@ -4,7 +4,8 @@ module.exports.run = async (bot, message, args) => {
 
 
 
-  //let localArgs =  message.content.split(" ").slice(1).join(' ');
+  let localArgs =  message.content.split(" ").slice(1).join(' ');
+
   if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}host`) {
     await message.delete();
     let richEmbed = new Discord.RichEmbed()
@@ -13,12 +14,9 @@ module.exports.run = async (bot, message, args) => {
         .setDescription("**Description: **Host an event\n**Usage: **>host [event type] {[arguments]}\n**Example: **>host ssu {host:@Rexhawk} {msg:lets get active!}");
     return message.channel.send(richEmbed);
   } else {
-    let testMsg = localArgs.search("test");
-    if (testMsg === 0) {
-      return await message.reply("Found string")
-    } else {
-      return await message.reply("String not found " + testMsg)
-    }
+    let arguments = localArgs.split(" {")
+
+    return await message.reply(arguments);
   }
 
   /*
