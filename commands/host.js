52968164024c0d7977199d2dd8a884766f473268
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-04-07 20:08:18 -0400

    host.js

--- commands/host.js
+++ commands/host.js
@@ -2,60 +2,23 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    if (String(message.content.split(" ")) === `${require("./../botconfig").prefix}join`) {
-        await message.delete();
-        let richEmbed = new Discord.RichEmbed()
-            .setColor("#333333")
-            .setTitle("**Command:** >join")
-            .setDescription("**Description: **Join an optional role\n**Usage: **>join [role]\n**Example: **>join ssu");
-        return message.channel.send(richEmbed);
-    }
-    if (String(args[0]).toLowerCase() === "ssu" || String(args[0]).toLowerCase() === "qotd") {
-        let allowedRole;
-        if (String(args[0]).toLowerCase() === "ssu") {
-            allowedRole = message.guild.roles.find("name", "SSU");
-        } else {
-            allowedRole = message.guild.roles.find("name", "QOTD");
-        }
+  /*
+    if(message.member.roles.find(r => r.name === "Administrative Services") || message.member.roles.find(r => r.name === "Cabinet") || message.member.roles.find(r => r.name === "Department Command") ||message.member.roles.find(r => r.name === "Mayor") || message.member.roles.find(r => r.name === "Assistant Mayor")){
         if (String(message.guild.id) === "621475864546115611") {
-            if (message.member.roles.find(r => r.name === "SSU")) {
-                if (String(args[0]).toLowerCase() === "ssu") {
-                    return message.reply(`**ERROR**: You already have the ssu role. If you wish to remove it, say \`>leave ssu\``);
-                }
-            } else {
-                if (String(args[0]).toLowerCase() === "ssu") {
-                    const guildMember = message.member;
-                    try {
-                        guildMember.addRole(allowedRole);
-                        return message.reply(`You have successfully joined \`SSU\`.`);
-                    } catch (err) {
-                        return message.reply(`**ERROR: ** ${err}`)
-                    }
-                }
-            }
-            if (message.member.roles.find(r => r.name === "QOTD")) {
-                if (String(args[0]).toLowerCase() === "qotd") {
-                    return message.reply(`**ERROR**: You already have the qtod role. If you wish to remove it, say \`>leave qotd\``);
-                }
-            } else {
-                if (String(args[0]).toLowerCase() === "qotd") {
-                    const guildMember = message.member;
-                    try {
-                        guildMember.addRole(allowedRole);
-                        return message.reply(`You have successfully joined \`QOTD\`.`);
-                    } catch (err) {
-                        return message.reply(`**ERROR: ** ${err}`)
-                    }
-                }
-            }
+            await message.delete();
+            return await bot.channels.get("636379801829244932").send(`<@&636656216587632640> **Server Startup!**\n**This Server Startup is hosted by: **${message.member}\n\n**Game Link:** https://www.roblox.com/games/4240465985/King-City-California\n\n*Remember to follow the rules and have fun.*`);
         } else {
+            await message.delete();
             return message.reply(`**ERROR**: \`Incorrect Server\``);
         }
     } else {
-        return message.reply(`**ERROR**: \`Role not found or is not optional.\``);
-    }
+        await message.delete();
+        return message.reply(`**ERROR**: \`Incorrect Permissions\``);
+    } */
+
+    return await message.reply(message);
 };
 
 module.exports.help = {
-    name: "join"
+    name: "host"
 };
