e15c1b7fdaf402da212bb242d33b656b85afb3e0
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-05-03 13:40:12 -0400

    reconfiurations

---
+++ commands/readdata.js
@@ -0,0 +1,21 @@
+const Discord           = require("discord.js");
+const GoogleSpreadsheet = require("google-spreadsheet");
+const { promisify }     = require("util");
+const creds             = require("./client_secret.json");
+
+async function accessSpreadsheet() {
+  const doc = new GoogleSpreadsheet("1jxHUUtuGA6UdrwhQG44oo4M1eJwScMN7qztZCKH37ms");
+  await promisify(doc.useServiceAccountAuth)(creds);
+
+  const info = await promisify(doc.getInfo)();
+  const sheet = info.worksheets[0];
+  console.log(`Title: ${shoot.title}`)
+}
+
+module.exports.run = async (bot, message, args) => {
+    accessSpreadsheet();
+};
+
+module.exports.help = {
+    name: "readdata"
+};
