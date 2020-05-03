efd80861bcd9c2afc4687537518ad2916ba5e5e8
rexhawk <53449265+rexhawkRBLX@users.noreply.github.com>
2020-05-03 14:25:39 -0400

    Update readdata.js

--- commands/readdata.js
+++ commands/readdata.js
@@ -3,14 +3,19 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 
 const doc = new GoogleSpreadsheet("1jxHUUtuGA6UdrwhQG44oo4M1eJwScMN7qztZCKH37ms");
 
-await doc.useServiceAccountAuth({
-  client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-  private_key: process.env.GOOGLE_PRIVATE_KEY,
-});
+async function accessSpreadsheet() {
+  await doc.useServiceAccountAuth({
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
+  });
+  await doc.loadInfo(); // loads document properties and worksheets
+  console.log(doc.title);
+}
+
+
 
 module.exports.run = async (bot, message, args) => {
-    await doc.loadInfo(); // loads document properties and worksheets
-    console.log(doc.title);
+  accessSpreadsheet();
 };
 
 module.exports.help = {
